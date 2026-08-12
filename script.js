d4356c6dbeaaeb5a8f8b0a5b8c87f68f5d36eb47
DIASSIVI-sys <nsangoudjenna1@gmail.com>
2026-08-12 15:04:21 +0100

    fix:réalisation de la page style (navigation)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+const produitsGrid=document.getElementById("produit-grid");
+
+/**  */
+async function loadProduits(){
+    try{
+        //Afficher un indicateur de chargement 
+        produitsGrid.innerHTML=" <p>Chargement des produits en cours </p>";
+        
+        const response = byIdOrFetch = await fetch('https://fakestoreapi.com/products');
+
+    }
+
+}
